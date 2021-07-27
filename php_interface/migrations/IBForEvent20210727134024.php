74b0b849cb6849e7076bf7ffa18136b8cd56b408
ararakys <67708496+ararakys@users.noreply.github.com>
2021-07-27 14:59:38 +0300

    Add files via upload

---
+++ php_interface/migrations/IBForEvent20210727134024.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class IBForEvent20210727134024 extends Version
+{
+    protected $description = "ИБ для события";
+
+    protected $moduleVersion = "3.28.8";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+        $helper->Iblock()->saveIblockType(array (
+  'ID' => 'event',
+  'SECTIONS' => 'Y',
+  'EDIT_FILE_BEFORE' => '',
+  'EDIT_FILE_AFTER' => '',
+  'IN_RSS' => 'N',
+  'SORT' => '500',
+  'LANG' => 
+  array (
+    'ru' => 
+    array (
+      'NAME' => 'Событие',
+      'SECTION_NAME' => '',
+      'ELEMENT_NAME' => '',
+    ),
+    'en' => 
+    array (
+      'NAME' => 'Event',
+      'SECTION_NAME' => '',
+      'ELEMENT_NAME' => '',
+    ),
+  ),
+));
+        $iblockId = $helper->Iblock()->saveIblock(array (
+  'IBLOCK_TYPE_ID' => 'event',
+  'LID' => 
+  array (
+    0 => 's1',
+  ),
+  'CODE' => 'y_event',
+  'API_CODE' => NULL,
+  'NAME' => 'YCобытие',
+  'ACTIVE' => 'Y',
+  'SORT' => '500',
+  'LIST_PAGE_URL' => '#SITE_DIR#/event/index.php?ID=#IBLOCK_ID#',
+  'DETAIL_PAGE_URL' => '#SITE_DIR#/event/detail.php?ID=#ELEMENT_ID#',
+  'SECTION_PAGE_URL' => '#SITE_DIR#/event/list.php?SECTION_ID=#SECTION_ID#',
+  'CANONICAL_PAGE_URL' => '',
+  'PICTURE' => NULL,
+  'DESCRIPTION' => '',
+  'DESCRIPTION_TYPE' => 'text',
+  'RSS_TTL' => '24',
+  'RSS_ACTIVE' => 'Y',
+  'RSS_FILE_ACTIVE' => 'N',
+  'RSS_FILE_LIMIT' => NULL,
+  'RSS_FILE_DAYS' => NULL,
+  'RSS_YANDEX_ACTIVE' => 'N',
+  'XML_ID' => NULL,
+  'INDEX_ELEMENT' => 'Y',
+  'INDEX_SECTION' => 'Y',
+  'WORKFLOW' => 'N',
+  'BIZPROC' => 'N',
+  'SECTION_CHOOSER' => 'L',
+  'LIST_MODE' => '',
+  'RIGHTS_MODE' => 'S',
+  'SECTION_PROPERTY' => 'N',
+  'PROPERTY_INDEX' => 'N',
+  'VERSION' => '2',
+  'LAST_CONV_ELEMENT' => '0',
+  'SOCNET_GROUP_ID' => NULL,
+  'EDIT_FILE_BEFORE' => '',
+  'EDIT_FILE_AFTER' => '',
+  'SECTIONS_NAME' => 'Разделы',
+  'SECTION_NAME' => 'Раздел',
+  'ELEMENTS_NAME' => 'Элементы',
+  'ELEMENT_NAME' => 'Элемент',
+  'EXTERNAL_ID' => NULL,
+  'LANG_DIR' => '/',
+  'SERVER_NAME' => '',
+  'ELEMENT_ADD' => 'Добавить элемент',
+  'ELEMENT_EDIT' => 'Изменить элемент',
+  'ELEMENT_DELETE' => 'Удалить элемент',
+  'SECTION_ADD' => 'Добавить раздел',
+  'SECTION_EDIT' => 'Изменить раздел',
+  'SECTION_DELETE' => 'Удалить раздел',
+));
+        $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Событие',
+  'ACTIVE' => 'Y',
+  'SORT' => '500',
+  'CODE' => 'EVENT',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'S',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '2',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => NULL,
+  'HINT' => '',
+));
+            $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Тема',
+  'ACTIVE' => 'Y',
+  'SORT' => '500',
+  'CODE' => 'THEME',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'S',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '2',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => NULL,
+  'HINT' => '',
+));
+            $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Автор',
+  'ACTIVE' => 'Y',
+  'SORT' => '500',
+  'CODE' => 'AUTHOR',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'S',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '2',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => NULL,
+  'HINT' => '',
+));
+    
+    }
+
+    public function down()
+    {
+        $helper = $this->getHelperManager();
+
+        $iblockId = $helper->Iblock()->getIblockIdIfExists('y_event', 'event');
+
+
+        $helper->Iblock()->deletePropertyIfExists($iblockId, 'EVENT');
+        $helper->Iblock()->deletePropertyIfExists($iblockId, 'THEME');
+        $helper->Iblock()->deletePropertyIfExists($iblockId, 'AUTHOR');
+
+        $helper->Iblock()->deleteIblockIfExists('y_event');
+        $helper->Iblock()->deleteIblockTypeIfExists('event');
+    }
+}
